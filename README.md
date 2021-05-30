7a71567e42280c93224f7b7a9b8053589ab4903a
Naruki Yoshikawa <naruki.yoshikawa@gmail.com>
2021-05-30 20:07:50 +0900

    Use Graph GA

---
+++ README.md
@@ -0,0 +1,6 @@
+# Let's design a blockbuster with genetic algorithm!
+
+Source code of http://genetic-blockbuster.herokuapp.com/
+
+The molecule is evolved using [Graph-based genetic algorithm](http://dx.doi.org/10.1039/C8SC05372C).
+I used the implementation from [GuacaMol Baselines](https://github.com/BenevolentAI/guacamol_baselines)
--- app.py
+++ app.py
@@ -1,73 +1,18 @@
 # -*- coding: utf-8 -*-
-import copy
-import nltk
 import json
 
 from flask import Flask, render_template, jsonify, request
-import numpy as np
-
 from rdkit import Chem
 from rdkit import rdBase
 
-import cfg_util
-import zinc_grammar
+import crossover as co
+import mutate as mu
 
-rdBase.DisableLog('rdApp.error')
-GCFG = zinc_grammar.GCFG
 # app.py
 app = Flask(__name__)
 
-def CFGtoGene(prod_rules, max_len=-1):
-    gene = []
-    for r in prod_rules:
-        lhs = GCFG.productions()[r].lhs()
-        possible_rules = [idx for idx, rule in enumerate(GCFG.productions())
-                          if rule.lhs() == lhs]
-        gene.append(possible_rules.index(r))
-    if max_len > 0:
-        if len(gene) > max_len:
-            gene = gene[:max_len]
-        else:
-            gene = gene + [np.random.randint(0, 256)
-                           for _ in range(max_len-len(gene))]
-    return gene
-
-
-def GenetoCFG(gene):
-    prod_rules = []
-    stack = [GCFG.productions()[0].lhs()]
-    for g in gene:
-        try:
-            lhs = stack.pop()
-        except Exception:
-            break
-        possible_rules = [idx for idx, rule in enumerate(GCFG.productions())
-                          if rule.lhs() == lhs]
-        rule = possible_rules[g % len(possible_rules)]
-        prod_rules.append(rule)
-        rhs = filter(lambda a: (type(a) == nltk.grammar.Nonterminal)
-                     and (str(a) != 'None'),
-                     zinc_grammar.GCFG.productions()[rule].rhs())
-        stack.extend(list(rhs)[::-1])
-    return prod_rules
-
-
-def mutation(gene):
-    idx = np.random.choice(len(gene))
-    gene_mutant = copy.deepcopy(gene)
-    gene_mutant[idx] = np.random.randint(0, 256)
-    return gene_mutant
-
-
-def canonicalize(smiles):
-    mol = Chem.MolFromSmiles(smiles)
-    if smiles != '' and mol is not None and mol.GetNumAtoms() > 1:
-        return Chem.MolToSmiles(mol)
-    else:
-        return smiles
-
-lsmi = canonicalize('CC(=O)Nc1c2n(c3ccccc13)C[C@](C)(C(=O)NC1CCCCC1)N(C1CCCCC1)C2=O')
-rsmi = canonicalize('O=[N+]([O-])c1c(Nc2cccc3ncccc23)ncnc1N1CCN(c2cccc(Cl)c2)CC1')
+lsmi = 'N#CC1=C(SCC(=O)Nc2cccc(Cl)c2)N=C([O-])[C@H](C#N)C12CCCCC2'
+rsmi = 'O=[N+]([O-])c1c(Nc2cccc3ncccc23)ncnc1N1CCN(c2cccc(Cl)c2)CC1'
 generation = 0
 
 @app.route('/')
@@ -83,24 +28,17 @@ def start():
     }
     return jsonify(values=json.dumps(return_json))
 
-def get_mutant(smiles):
-    new_smiles = ""
-    gene = CFGtoGene(cfg_util.encode(smiles), max_len=300)
-    while True:
-        gene = CFGtoGene(cfg_util.encode(smiles), max_len=300)
-        mutated_gene = mutation(gene)
-        new_smiles = cfg_util.decode(GenetoCFG(mutated_gene))
-        new_mol = Chem.MolFromSmiles(new_smiles)
-        if len(new_smiles) < 15 or new_mol is None:
-            continue
+def mutate(smiles):
+    mol = Chem.MolFromSmiles(smiles)
+    mutant = None
+    while mutant is None:
+        mutant = mu.mutate(mol, 1.0)
         try:
-            Chem.SanitizeMol(new_mol)
+            Chem.SanitizeMol(mutant)
         except:
             continue
-        new_smiles = Chem.MolToSmiles(new_mol)
-        if new_smiles != smiles:
-            break
-    return new_smiles
+    return Chem.MolToSmiles(mutant)
+
 
 @app.route('/mutate', methods=['POST'])
 def show():
@@ -109,9 +47,10 @@ def show():
     global generation
 
     generation += 1
-    smiles = canonicalize(request.json["smiles"])
-    lsmi = get_mutant(smiles)
-    rsmi = get_mutant(smiles)
+    smiles = request.json["smiles"]
+
+    lsmi = mutate(smiles)
+    rsmi = mutate(smiles)
 
     return_json = {
         "leftSmiles": lsmi,
@@ -120,7 +59,6 @@ def show():
     }
 
     return jsonify(values=json.dumps(return_json))
-
 
 if __name__ == '__main__':
     app.run(debug=True)
